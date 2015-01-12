e9aa7c9367e1984cc72809a7b52e2b2862dea6b8
Philip M. Gollucci <pgollucci@p6m7g8.com>
2015-01-12 13:27:41 -0500

    ansible/packer

--- bin/cellar.sh
+++ bin/cellar.sh
@@ -7,13 +7,13 @@ brew update
 
 brew tap homebrew/versions
 brew tap homebrew/dupes
-brew tap homebrew/binaries
+brew tap homebrew/binary
 
 for pkg in git hub irssi nmap rdesktop swaks xz; do
   brew install $pkg
 done
 
-for pkg in ilibyaml pyenv; do
+for pkg in ilibyaml pyenv packer ansible; do
   brew install $pkg
 done
 
