a24a54cd53f6b123de84ed32baedcf28157136a6
Philip M. Gollucci <pgollucci@p6m7g8.com>
2016-03-28 18:07:30 -0400

    bind a key

--- .emacs.d/personal/markdown-setup.el
+++ .emacs.d/personal/markdown-setup.el
@@ -2,8 +2,11 @@
 (package 'markdown-mode+)
 (package 'gh-md)
 (package 'markdown-toc)
+
 (autoload 'markdown-mode "markdown-mode"
    "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+(global-set-key (kbd "C-x gb") 'gh-md-render-buffer)
