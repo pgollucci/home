4b99ad56996ac4e9af6fc1035d347954b9ebed15
Philip M. Gollucci <pgollucci@p6m7g8.com>
2016-03-28 21:35:58 -0400

    convert to Brewfile

--- bin/cellar.sh
+++ bin/cellar.sh
@@ -9,34 +9,10 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew doctor
 brew update
 
-brew tap homebrew/versions
-brew tap homebrew/dupes
-brew tap homebrew/binary
-brew tap homebrew/x11
-brew tap caskroom/cask
+brew tap homebrew/bundle
 
-## src distribution apps
-base_pkgs="ctags-exuberant gpg go subversion hub htop-osx irssi nmap swaks sqlite xz graphiz rdesktop"
-devel_pkgs="autoconf automake"
-db_pkgs="mysql postgresql innotop redis memcache"
-devops_pkgs="awscli packer ansible docker boot2docker"
-
-for pkg in $base_pkgs $devel_pkgs $db_pkgs $devops_pkgs; do
-  brew install $pkg
-done
-
-brew install git --without-completions
-brew install git-extras
-
-## binary distribution apps
-binary_pkgs="alfred chefdk dash evernote firefox gitx google-chrome google-drive google-hangouts \
-	hipchat microsoft-lync omnigraffle iterm2 java r synergy skitch squidman  vagrant virtualbox \
-	utorrent vlc xquartz wireshark wkhtmltopdf"
-brew install brew-cask
-for pkg in $binary_pkgs; do
-  brew cask install $pkg
-done
+brew bundle --file=$HOMR/bin/Brewfile
 
 brew prune
 
-sudo rm -f /usr/bin/ctags
+sudo rm -rf /usr/bin/ctags
