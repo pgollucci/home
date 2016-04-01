09df99f0cea9d51f89dd1f112ce570c736a81d99
Philip M. Gollucci <pgollucci@p6m7g8.com>
2016-04-01 12:59:12 -0400

    clean up prompt

--- .zsh/modules/aws/cfg.zsh
+++ .zsh/modules/aws/cfg.zsh
@@ -12,16 +12,23 @@ aws_clear() {
 
 aws_prompt_info() {
 
+  aws_sts_prompt_info
+  aws_target_prompt_info
+}
+
+aws_target_prompt_info() {
+
   if [ -n "$AWS_PROFILE" ]; then
     echo "[$AWS_PROFILE($AWS_ENV/$ENV_LEVEL) - $AWS_REGION($AWS_VPC)]"
   fi
 
 }
 
-sts_prompt_info() {
+aws_sts_prompt_info() {
 
     local creds=$HOME/.aws/credentials
 
+    echo "${magenta}aws:${norm}\c"
     [ -e $creds ] || return
 
     local mtime=$(command stat -f "%m" $creds)
@@ -29,9 +36,9 @@ sts_prompt_info() {
     local diff=$(($now-$mtime))
 
     if [ $diff -gt 3500 ]; then
-	echo "${magenta}sts:   ${red}$diff${norm}s"
+	echo "   ${magenta}sts [${red}$diff${norm}s] \c"
     else
-	echo "${magenta}sts:   ${green}$diff${norm}s"
+	echo "   ${magenta}sts [${green}$diff${norm}s] \c"
     fi
 }
 
