6e3d4a3de4ca57720173d00592a30bdadc678726
Philip M. Gollucci <pgollucci@p6m7g8.com>
2016-05-05 17:26:09 -0400

    new theme

--- .emacs.d/personal/theme.el
+++ .emacs.d/personal/theme.el
@@ -1,9 +1,12 @@
-(load-theme 'wombat)
+;(load-theme 'wombat)
 
-(set-cursor-color "orange")
-(set-face-attribute 'region nil :background "cornflower blue")
-(set-face-attribute 'isearch nil :background "gold" :foreground "black")
-(set-face-attribute 'lazy-highlight nil :background "dark goldenrod" :foreground "white")
+;(set-cursor-color "orange")
+;(set-face-attribute 'region nil :background "cornflower blue")
+;(set-face-attribute 'isearch nil :background "gold" :foreground "black")
+;(set-face-attribute 'lazy-highlight nil :background "dark goldenrod" :foreground "white")
+
+(package 'alect-themes)
+(load-theme 'alect-black)
 
 (when (fboundp 'tool-bar-mode)
   (tool-bar-mode 0))
