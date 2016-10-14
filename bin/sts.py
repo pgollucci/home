12bb19a23f405abe2f60e843f9946c1748e4e50c
Philip M. Gollucci <pgollucci@p6m7g8.com>
2016-10-14 16:35:05 -0400

    sync

--- bin/sts.py
+++ bin/sts.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 import sys
 import boto.sts
@@ -33,15 +33,13 @@ awsconfigfile = '/.aws/credentials'
 sslverification = True
 
 # idpentryurl: The initial URL that starts the authentication process.
-idpentryurl = 'https://accounts.google.com/o/saml2/initsso?idpid=C0237ok4z&spid=287993144883&forceauthn=false'
+idpentryurl = 'https://sso.jumpcloud.com/saml2/aws-p6'
 
 ##########################################################################
 
 # Get the federated credentials from the user
-#username = raw_input()
-#password = getpass.getpass()
-username='pgollucci@p6m7g8.com'
-password='pgabc123!'
+username = raw_input()
+password = getpass.getpass()
 
 # Initiate session handler
 session = requests.Session()
@@ -52,24 +50,13 @@ session.auth = HttpNtlmAuth(username, password, session)
 
 user_agent = {'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36'}
 cookies = {
-  'GALX': '0MR3vsc-yoc',
-  'ACCOUNT_CHOOSER': 'AFx_qI5__tRrap-XsHRdEdXwYmFyJ65ZGIWbDAgTUubsA-insGNprYo9_uLqZGo_XlZk1ldsd2JIRoUn7VPbUYPdeQWMXItFTFtEmoB5Kqf_4l1dci5OCXPfExz7IaVOjfobTyYtlweqg3Di3-CqDJ5-VkMHrhzjKw',
-  'GMAIL_RTT': '286',
-  'GAPS': '1:XuAPDl7O-NCLmgKPoT9wd0LZ0ACVWQ:PxYhXUoT9T-pLz6Y',
-  'SID': '1ANaoCYjMwIXGEB4wznLI_MqrPu9n-aa7mmlaWKVQjeliE5SccD3Dp0DUnB8m3Ea9aXw5Q.',
-  'LSID': 'mail|o.mail.google.com|s.youtube|ss:1ANaoPyfvBn-rVhwmxOict9pfkpsRV1v5tcKQhOgf7qFjhTyLUWsgB1rx6XztyWxU-3YqA.',
-  'HSID': 'ANPkjPmCNYboiBKUs',
-  'SSID': 'ARqT-Mff-g8SY0IzB',
-  'APISID': '8CFCl0B2N_fjPRzy/ALeWRrvRNbSIuYoQ1',
-  'SAPISID': '0Waj1keUYkb5y_XJ/A6qtJ9xu4Zt2q0Vsj',
-  'NID': '87=Voj755zbCCzrXnM8rs4VL3yYyN-bNpJJhZInDk_xlqTOamBtoXzC3unuS8hkj68aSqoSrL_-bovgA4cJ-U6SKrSx0VrQ3oaw5KhzYZkp1WChgm6_pfShyrQv5Fb8_YkDa8dBeob9tBomRKZx_cVptjfHyAALUp5Fp0p14wzzWhBlN4-YF8wy9VpO2jKOiA-jmE0tq3xadzxidb4g3tcBpPWQmcYv7IKSsws'
 }
 
 # Opens the initial AD FS URL and follows all of the HTTP302 redirects
 response = session.get(idpentryurl, verify=sslverification, headers = user_agent, cookies=cookies)
 
 # Debug the response if needed
-#print (response.text)
+print (response.text)
 
 # Overwrite and delete the credential variables, just for safety
 username = '##############################################'
