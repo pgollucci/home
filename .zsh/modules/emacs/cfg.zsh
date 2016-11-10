895a682c285cca97e505eef36ab088f38c9670f6
Philip M. Gollucci <pgollucci@p6m7g8.com>
2016-11-09 20:40:47 -0500

    make cocoa work again

--- .zsh/modules/emacs/cfg.zsh
+++ .zsh/modules/emacs/cfg.zsh
@@ -5,15 +5,7 @@ __setup() {
 
 emacs_wrapper() {
 
-  if [ -z "$EMACS_X" -o "$EMACS_X" = "nil" ]; then
-      # Create one if there is no X window yet.
-      local x=`emacsclient --alternate-editor '' --eval '(x-display-list)' 2>/dev/null`
-      export EMACS_X=$x
-      emacsclient --alternate-editor "" --create-frame "$@"
-  else
-      # prevent creating another X frame if there is at least one present.
-      emacsclient --alternate-editor "" "$@"
-  fi
+  /usr/local/bin/emacsclient --alternate-editor '' --create-frame "$@"
 }
 
 __setup
