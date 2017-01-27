0f93c1ca7f4824c8cb23433bf5f1bfef864fa855
Philip M. Gollucci <pgollucci@p6m7g8.com>
2017-01-27 12:54:42 -0500

    clean up

--- bin/hcellar.sh
+++ bin/hcellar.sh
@@ -1,16 +1,25 @@
 #!/bin/sh -x
 
 # XXX: Doesn't install OneDrive, SQLPro for MSSQL
+main() {
 
-sudo rm -rf /usr/local /Library/Caches/Homebrew /opt/homebrew-cask ~/Applications
+    sudo -v
 
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    # Keep-alive: update existing sudo time stamp if set, otherwise do nothing.
+    while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-brew doctor
-brew update
+    sudo rm -rf /usr/local /Library/Caches/Homebrew /opt/homebrew-cask ~/Applications
 
-brew tap homebrew/bundle
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew bundle --file=$HOME/bin/Brewfile
+    brew doctor
+    brew update
 
-brew prune
+    brew tap homebrew/bundle
+
+    brew bundle --file=$HOME/bin/Brewfile
+
+    brew prune
+}
+
+main "$@"
