e1936c96ce38ecbddcbd454797fdf8864b4e6fb7
Philip M. Gollucci <pgollucci@p6m7g8.com>
2017-03-23 10:26:47 -0400

    github additions

--- .zsh/modules/github/cfg.zsh
+++ .zsh/modules/github/cfg.zsh
@@ -1,12 +1,18 @@
 __setup() {
 
+    GITHUB_HOST=github.com
+    GITHUB_URL=https://${GITHUB_HOST}
+    GITHUB_API_HOST=api.${GITHUB_HOST}
+    GITHUB_API_URL=https://api.${GITHUB_HOST}
+    GITHUB_API_VER=v3
 }
 
 gh_paginate() {
     local url="$1"
     local auth="$2"
 
-    local file=/tmp/gh.pagination.txt
+    local dir=$(mktemp -d -t gh.pagination)
+    local file=${dir}/output.txt
 
     rm -f $file
     curl -i -s $(echo $auth) "$url" >> $file
@@ -29,7 +35,7 @@ gh_paginate() {
     done
 
     cat $file
-    rm -f $file
+    rm -f $dir
 }
 
 gh_list_all_orgs() {
@@ -83,6 +89,93 @@ gh_clone_or_pull_repo() {
 	mkdir -p $dir/$org/$repo
 	(cd $dir/$org ; git clone -q --depth 1 ${gh}/${org}/${repo}.git > /dev/null)
     fi
+}
+
+git_setup_dir() {
+    local uri="$1"
+    local ver="$2"
+    local repo="$3"
+
+    local safe_repo=$(echo $repo | sed -e 's,/,_,g')
+
+    local prefix="${safe_repo}.XXX"
+
+    local dir=$(mktemp -d -t $prefix)
+    (
+	cd ${dir}
+	git clone -q ${uri} .
+	if [ -n "${ver}" ]; then
+	    git checkout -q ${ver}
+	fi
+    )
+
+    echo ${dir}
+}
+
+git_commit_file_to() {
+    [ -z "${TEST_MODE}" ] || return
+
+    local repo="$1"
+    local original="$2"
+    local dir="$3"
+    local file="$4"
+    local msg="$5"
+
+    gh_or_fp "${repo}"
+    local clone_dir=$(git_setup_dir "${GH}" "" "${repo}")
+
+    mkdir -p ${clone_dir}/${dir}
+    cp ${original} ${clone_dir}/${dir}/${file}
+    (
+	cd ${clone_dir}
+	git add ${dir}/${file}
+	git commit -m "${msg}"
+	git push
+    ) > /dev/null
+    rm -rf ${clone_dir}
+}
+
+gh_or_fp() {
+    local thing="$1"
+
+    if [[ ${thing} = /* ]]; then
+	GH_REPO=
+	GH=
+	GH_VER=
+    else
+	local org_proj=${thing%%%*}
+	GH_REPO=${org_proj}
+	GH="${GITHUB_URL}${org_proj}"
+	if [[ ${thing} = *%* ]]; then
+	    GH_VER=${thing##*%}
+	else
+	    GH_VER=master
+	fi
+    fi
+}
+
+gh_oauth_token_get() {
+    local gh_api="$1"
+    local user="$2"
+    local pass="$3"
+    local note="$4"
+
+    local output=$(curl -X POST -s -u ${user}:${pass} -d '{"scopes": ["repo", "user"], "note": "'${note}'"}' ${gh_api}/v3/authorizations  | egrep '"token"|"id"')
+
+    local id=$(echo $output | awk '{ print $2 }' | sed -e 's/[", ]//g')
+    local token=$(echo $output | awk '{ print $4 }' | sed -e 's/[", ]//g')
+
+    GH_TOKEN=$token
+    GH_TOKEN_ID=$id
+}
+
+gh_oauth_token_del() {
+    local gh_api="$1"
+    local user="$2"
+    local pass="$3"
+    local id="$4"
+
+    curl -X DELETE -s -u ${user}:${pass} ${gh_api}/v3/authorizations/$id
 }
 
 __setup
