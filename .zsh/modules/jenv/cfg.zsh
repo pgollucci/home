97c32f46abd2ccd19da32bf0bdcab5ccd44c09db
Philip M. Gollucci <pgollucci@p6m7g8.com>
2017-03-26 21:56:46 -0400

    speed up

--- .zsh/modules/jenv/cfg.zsh
+++ .zsh/modules/jenv/cfg.zsh
@@ -1,11 +1,13 @@
 __setup() {
 
-    export JENV_ROOT=$HOME/.repos/langs/jenv
-    path_if $JENV_ROOT/bin
+    if [ -n "$JENV_MANUAL" ]; then
+	export JENV_ROOT=$HOME/.repos/langs/jenv
+	path_if $JENV_ROOT/bin
 
-    if [ -x $JENV_ROOT/bin/jenv ]; then
-      export HAS_JENV=1
-      eval "$(jenv init - )"
+	if [ -x $JENV_ROOT/bin/jenv ]; then
+	    export HAS_JENV=1
+	    eval "$(jenv init - )"
+	fi
     fi
 }
 
@@ -16,15 +18,19 @@ jenv_prompt_line() {
 
 jenv_version() {
 
-    if [ $HAS_JENV ]; then
-	if [ -n "$JENV_VERSION" ]; then
-	    echo "$JENV_VERSION"
+    if [ -n "$JENV_MANUAL" ]; then
+	if [ $HAS_JENV ]; then
+	    if [ -n "$JENV_VERSION" ]; then
+		echo "$JENV_VERSION"
+	    else
+		local ver=$(jenv version | awk '{print $1}')
+		echo "$ver"
+	    fi
 	else
-	    local ver=$(jenv version | awk '{print $1}')
-	    echo "$ver"
+	    echo "system:$(java -version 2>&1|awk '/Environment/{print}' | cut -d' ' -f 2,6- |sed -e 's,),,')"
 	fi
     else
-	echo "system:$(java -version 2>&1|awk '/Environment/{print}' | cut -d' ' -f 2,6- |sed -e 's,),,')"
+	echo off
     fi
 }
 
@@ -32,6 +38,5 @@ jev() {
 
     eval `jenv shell $1`
 }
-
 
 __setup
