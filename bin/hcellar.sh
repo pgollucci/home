5c2d1c3c22c664335f8586fa73adaa1747e03bd2
Philip M. Gollucci <pgollucci@p6m7g8.com>
2017-06-30 13:19:37 -0400

    clean casks

--- bin/hcellar.sh
+++ bin/hcellar.sh
@@ -1,4 +1,14 @@
-#!/bin/sh -x
+#!/bin/sh
+
+remove_casks() {
+
+    local formuli=$(grep ^cask bin/Brewfile | awk '{print $2}' | sed -e "s,',,g")
+
+    local formula
+    for formula in $formuli; do
+	brew cask uninstall $formula
+    done
+}
 
 # XXX: Doesn't install OneDrive, SQLPro for MSSQL
 main() {
@@ -8,9 +18,11 @@ main() {
     # Keep-alive: update existing sudo time stamp if set, otherwise do nothing.
     while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+    remove_casks
+
     sudo rm -rf /usr/local /Library/Caches/Homebrew /opt/homebrew-cask ~/Applications
 
-    yes | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    yes | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
     export PATH=/usr/local/bin:${PATH}
     brew doctor
@@ -18,8 +30,7 @@ main() {
 
     brew tap homebrew/bundle
 
-    brew bundle --file=$HOME/bin/Brewfile
-
+    brew bundle --file=$HOME/bin/
     brew prune
 }
 
