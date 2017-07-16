a85b682a37686968d44194147ae3fe4b8ec32cf0
Philip M. Gollucci <pgollucci@p6m7g8.com>
2017-07-16 19:03:19 -0400

    add smore

--- bin/hcpan.sh
+++ bin/hcpan.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 cpan -T Log::Log4perl
+cpan -T HTTP::Date
+cpan -T YAML
 cpan -T CPAN
 cpan -T Template \
      LWP::UserAgent \
      JSON \
-     MIME::Lite
+     MIME::Lite \
+     Text::Replace
 
 plenv rehash
