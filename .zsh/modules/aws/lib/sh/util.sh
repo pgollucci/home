dfe45c4fa0d0f2e63e6e8458dea53bb24e03ee83
Philip M. Gollucci <pgollucci@p6m7g8.com>
2017-07-28 21:51:01 -0400

    fixes

--- .zsh/modules/aws/lib/sh/util.sh
+++ .zsh/modules/aws/lib/sh/util.sh
@@ -11,10 +11,8 @@ aws_include_service() {
 	local files=$(cd $pdir ; ls -1)
 
 	local file
-	for file in $files; do
-	    set -x
-	    DEBUG=1 load_file "$pdir/$file.sh"
-	    set +x
+	for file in $(echo $files); do
+	    DEBUG=1 load_file "$pdir/$file"
 	done
     done
 }
