451cfbd343f0508c31c286c31704fadea0a1b593
Philip M. Gollucci <pgollucci@p6m7g8.com>
2017-08-05 21:33:29 -0400

    fixes

--- bin/repos-asf.zsh
+++ bin/repos-asf.zsh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh -li
 
-gh_clone_org_repos "https://github.com" "https://api.github.com" "apache" "$HOME/dev/repos/gh/apache"
+gh_clone_org_repos "https://github.com" "https://api.github.com" "apache" "$HOME/dev/repos/gh"
--- bin/repos-fbsd.zsh
+++ bin/repos-fbsd.zsh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh -li
 
-gh_clone_org_repos "https://github.com" "https://api.github.com" "freebsd" "$HOME/dev/repos/gh/freebsd"
+gh_clone_org_repos "https://github.com" "https://api.github.com" "freebsd" "$HOME/dev/repos/gh"
