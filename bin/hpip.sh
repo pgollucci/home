6c97247f6cbb983311a49488104e336a9d7d4b6b
Philip M. Gollucci <pgollucci@p6m7g8.com>
2017-09-03 09:15:44 -0400

    functions

--- bin/hpip.sh
+++ bin/hpip.sh
@@ -1,15 +1,20 @@
 #!/bin/sh -x
 
-pyenv virtualenv 3.6.2 home
-pyenv local 3.6.2/envs/home
+main() {
+    pyenv virtualenv 3.6.2 home
+    pyenv local 3.6.2/envs/home
 
-pip install virtualenv
-pip install awscli
-pip install boto3
-pip install docopt
-pip install infi.docopt-completion
-pip install configparser
-pip install bs4
-pip install requests
+    pip install virtualenv
+    pip install awscli
+    pip install boto3
+    pip install docopt
+    pip install infi.docopt-completion
+    pip install configparser
+    pip install bs4
+    pip install requests
 
-pyenv rehash
+    pyenv rehash
+}
+
+main "$@"
+exit $?
