f6d82cb430c644c354aea0412ecda6490356ae34
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-04-09 18:44:15 -0400

    more defaults

--- bin/hdefaults.sh
+++ bin/hdefaults.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Battery
-defaults write com.apple.menuextra.battery ShowPercent -string "NO"
-defaults write com.apple.menuextra.battery ShowTime -string "NO"
+defaults write com.apple.menuextra.battery ShowPercent -string "YES"
+defaults write com.apple.menuextra.battery ShowTime -string "YESG"
 
 # KB
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
@@ -27,12 +27,24 @@ defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 defaults write com.apple.finder AppleShowAllFiles YES
 defaults write com.apple.finder EmptyTrashSecurely -bool true
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+defaults write com.apple.finder AppleShowAllFiles -bool true
+defaults write NSGlobalDomain   AppleShowAllExtensions -bool true
+defaults write com.apple.finder ShowStatusBar -bool true
+defaults write com.apple.finder ShowPathbar -bool true
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+chflags nohidden ~/Library
 
 # Dock
 defaults write com.apple.dock show-process-indicators -bool true
 defaults write com.apple.dock autohide -bool true
 
-
-
-
+# Other
+defaults write com.apple.CrashReporter DialogType -string "none"
 
