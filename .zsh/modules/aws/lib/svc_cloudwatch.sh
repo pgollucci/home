3c8a1df68c240abebaa36d32cfc7dc4fc7407597
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-06-16 19:42:00 -0400

    archive

--- .zsh/modules/aws/lib/svc_cloudwatch.sh
+++
@@ -1,7 +0,0 @@
-aws_cloudwatch_logs_group_make() {
-    local logs_group_name="$1"
-
-    aws logs create-log-group --log-group-name $logs_group_name
-
-    aws --output text logs describe-log-groups --log-group-name-prefix "$logs_group_name" --query "logGroups[].arn"
-}
