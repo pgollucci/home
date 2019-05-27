4b0d48ed86cf5bc37aa10687610ad9ddc16bc145
Philip M. Gollucci <pgollucci@p6m7g8.com>
2019-05-26 20:57:42 -0400

    inch forward

---
+++ init.zsh
@@ -0,0 +1,13 @@
+p6df::modules::home::version() { echo "0.0.1" }
+
+p6df::modules::home::home::symlink() {
+
+  ln -fs $P6_DFZ_SRC_DIR/pgollucci/home/.fullname .fullname
+  ln -fs $P6_DFZ_SRC_DIR/pgollucci/home/.organization .organization
+  ln -fs $P6_DFZ_SRC_DIR/pgollucci/home/.ssh .ssh
+  ln -fs $P6_DFZ_SRC_DIR/pgollucci/home/.signature .signature
+  ln -fs $P6_DFZ_SRC_DIR/pgollucci/home/.gnupg .gnupg
+
+  ln -fs $P6_DFZ_SRC_DIR/pgollucci/home/README.md README.md
+  ln -fs $P6_DFZ_SRC_DIR/pgollucci/home/LICENSE LICENSE
+}
