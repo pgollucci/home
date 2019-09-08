415cf28415ec62e2246db7cddbe21e4d28811304
Philip M. Gollucci <pgollucci@p6m7g8.com>
2019-09-08 12:27:53 -0400

    gpg

--- init.zsh
+++ init.zsh
@@ -2,13 +2,14 @@ p6df::modules::home::version() { echo "0.0.1" }
 
 p6df::modules::home::home::symlink() {
 
+  ln -fs $P6_DFZ_SRC_DIR/pgollucci/home/README.md README.md
+  ln -fs $P6_DFZ_SRC_DIR/pgollucci/home/LICENSE LICENSE
+
   ln -fs $P6_DFZ_SRC_DIR/pgollucci/home/.fullname .fullname
-  ln -fs $P6_DFZ_SRC_DIR/pgollucci/home/.gnupg .gnupg
   ln -fs $P6_DFZ_SRC_DIR/pgollucci/home/.hosts .hosts
   ln -fs $P6_DFZ_SRC_DIR/pgollucci/home/.organization .organization
   ln -fs $P6_DFZ_SRC_DIR/pgollucci/home/.signature .signature
-  ln -fs $P6_DFZ_SRC_DIR/pgollucci/home-private/ssh .ssh
 
-  ln -fs $P6_DFZ_SRC_DIR/pgollucci/home/README.md README.md
-  ln -fs $P6_DFZ_SRC_DIR/pgollucci/home/LICENSE LICENSE
+  ln -fs $P6_DFZ_SRC_DIR/pgollucci/home-private/ssh .ssh
+  ln -fs $P6_DFZ_SRC_DIR/pgollucci/home-private/gnupg .gnupg
 }
