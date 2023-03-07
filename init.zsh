1b44338ca4ef852256fad22c635e2b13f5e06f1f
Philip M. Gollucci <pgollucci@p6m7g8.com>
2023-03-06 21:54:24 -0500

    sync

--- init.zsh
+++ init.zsh
@@ -18,16 +18,3 @@ p6df::modules::home::home::symlink() {
   ln -fs $P6_DFZ_SRC_DIR/pgollucci/home-private/ssh .ssh
   ln -fs $P6_DFZ_SRC_DIR/pgollucci/home-private/gnupg .gnupg
 }
-
-######################################################################
-#<
-#
-# Function: p6df::modules::home::init()
-#
-#>
-######################################################################
-p6df::modules::home::init() {
-
-  p6df::util::cdpath::if "$P6_DFZ_SRC_DIR/pgollucci"
-  export HOMEBREW_GITHUB_API_TOKEN=$P6_GH_TOKEN
-}
