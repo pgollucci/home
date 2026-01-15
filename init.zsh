ee1b0c0e9f826d2a7c94ba7b40f91a5af5a1c812
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-01-15 15:30:27 -0500

    feat(profile): +aws (#20)

--- init.zsh
+++ init.zsh
@@ -37,15 +37,48 @@ p6df::modules::home::home::symlink() {
 #
 # Function: p6df::modules::home::profile::select::me()
 #
+#  Environment:	 P6_PROFILE
 #>
 ######################################################################
 p6df::modules::home::profile::select::me() {
 
   p6_env_export "P6_PROFILE" "arkestro"
 
+  p6df::modules::home::profile::1password
+  p6df::modules::home::profile::aws
+
+  p6_return_void
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::home::profile::1password()
+#
+#>
+######################################################################
+p6df::modules::home::profile::1password() {
+
   p6_1password_account_signin "my"
   p6_1password_whoami_email
   p6_1password_vault_select "Private"
 
   p6_return_void
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::home::profile::aws()
+#
+#  Environment:	 P6_AWS_ORG
+#>
+######################################################################
+p6df::modules::home::profile::aws() {
+
+   p6_env_export P6_AWS_ORG "p6"
+   p6_aws_cli_organization_on "$P6_AWS_ORG"
+   p6df::modules::aws::profiles::list
+
+   p6_return_void
 }
