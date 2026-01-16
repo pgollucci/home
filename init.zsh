6adda6c5a4d0d94a3bab10cd68102226e54ec328
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-01-15 22:24:23 -0500

    feat(api): 50% of home profile

--- init.zsh
+++ init.zsh
@@ -7,6 +7,7 @@
 ######################################################################
 p6df::modules::home::deps() {
   ModuleDeps=(
+    p6m7g8-dotfiles/p6df-aws
     p6m7g8-dotfiles/p61password
   )
 }
@@ -42,10 +43,21 @@ p6df::modules::home::home::symlink() {
 ######################################################################
 p6df::modules::home::profile::select::me() {
 
-  p6_env_export "P6_PROFILE" "arkestro"
+  p6_env_export "P6_PROFILE" "P6"
 
   p6df::modules::home::profile::1password
   p6df::modules::home::profile::aws
+  p6df::modules::home::profile::github
+  p6df::modules::home::profile::npm
+  p6df::modules::home::profile::launchdarkly
+  p6df::modules::home::profile::pagerduty
+  p6df::modules::home::profile::vscode
+  # chat gpt
+  # gemini
+  # claud code
+  # codex
+  # huggingface
+  # cursor
 
   p6_return_void
 }
@@ -55,9 +67,12 @@ p6df::modules::home::profile::select::me() {
 #
 # Function: p6df::modules::home::profile::1password()
 #
+#  Environment:	 P6_1PASSWORD_PROFILE
 #>
 ######################################################################
 p6df::modules::home::profile::1password() {
+
+  p6_env_export "P6_1PASSWORD_PROFILE" "Gollucci"
 
   p6_1password_account_signin "my"
   p6_1password_whoami_email
@@ -71,14 +86,101 @@ p6df::modules::home::profile::1password() {
 #
 # Function: p6df::modules::home::profile::aws()
 #
-#  Environment:	 P6_AWS_ORG
+#  Environment:	 P6_AWS_ORG P6_AWS_PROFILE
 #>
 ######################################################################
 p6df::modules::home::profile::aws() {
+
+   p6_env_export "P6_AWS_PROFILE" "p6m7g8"
 
    p6_env_export P6_AWS_ORG "p6"
    p6_aws_cli_organization_on "$P6_AWS_ORG"
    p6df::modules::aws::profiles::list
 
    p6_return_void
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::home::profile::github()
+#
+#  Environment:	 GH_CONFIG_DIR GH_TOKEN P6_GITHUB_PROFILE XXX
+#>
+######################################################################
+p6df::modules::home::profile::github() {
+
+  p6_env_export "P6_GITHUB_PROFILE" "Personal/pgollucci"
+
+  local user=$(p6_1password_cli_item_get_username "Github")
+  local token=$(p6_1password_cli_item_get_password "Github")
+
+  # GH_CONFIG_DIR ~/.config/gh
+  # XXX: Maintain symlinks like aws
+  p6_env_export "GH_TOKEN" "$token"
+
+  p6_return_void
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::home::profile::npm()
+#
+#  Environment:	 NPM NPM_TOKEN P6_NPM_PROFILE
+#>
+######################################################################
+p6df::modules::home::profile::npm() {
+
+  p6_env_export "P6_NPM_PROFILE" "Personal/pgollucci"
+
+  local token=$(p6_1password_cli_item_get_password "NPM")
+  p6_env_export NPM_TOKEN "$token"
+
+  p6_return_void
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::home::profile::launchdarkly()
+#
+#  Environment:	 P6_LAUNCHDARKLY_PROFILE
+#>
+######################################################################
+p6df::modules::home::profile::launchdarkly() {
+
+  p6_env_export "P6_LAUNCHDARKLY_PROFILE" "Personal/"
+
+  p6_return_void
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::home::profile::pagerduty()
+#
+#  Environment:	 P6_PAGERDUTY_PROFILE
+#>
+######################################################################
+p6df::modules::home::profile::pagerduty() {
+
+  p6_env_export "P6_PAGERDUTY_PROFILE" "Personal/"
+
+  p6_return_void
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::home::profile::vscode()
+#
+#  Environment:	 P6_VSCODE_PROFILE
+#>
+######################################################################
+p6df::modules::home::profile::vscode() {
+
+  p6_env_export "P6_VSCODE_PROFILE" "Personal"
+
+  p6_return_void
 }
