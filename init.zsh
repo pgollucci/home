a03366127f8cfcc6f43ccaf1c6879fca51c0fc9e
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-01-21 10:39:08 -0500

    feat: +p6df::modules::home::profile::{openai,claude,gemini} (#23)

--- init.zsh
+++ init.zsh
@@ -38,12 +38,12 @@ p6df::modules::home::home::symlink() {
 #
 # Function: p6df::modules::home::profile::select::me()
 #
-#  Environment:	 P6_PROFILE
+#  Environment:	 P6_DFZ_PROFILE
 #>
 ######################################################################
 p6df::modules::home::profile::select::me() {
 
-  p6_env_export "P6_PROFILE" "P6"
+  p6_env_export "P6_DFZ_PROFILE" "P6"
 
   p6df::modules::home::profile::1password
   p6df::modules::home::profile::aws
@@ -52,12 +52,9 @@ p6df::modules::home::profile::select::me() {
   p6df::modules::home::profile::launchdarkly
   p6df::modules::home::profile::pagerduty
   p6df::modules::home::profile::vscode
-  # chat gpt
-  # gemini
-  # claud code
-  # codex
-  # huggingface
-  # cursor
+  p6df::modules::home::profile::openai
+  p6df::modules::home::profile::gemini
+  p6df::modules::home::profile::claude
 
   p6_return_void
 }
@@ -67,12 +64,12 @@ p6df::modules::home::profile::select::me() {
 #
 # Function: p6df::modules::home::profile::1password()
 #
-#  Environment:	 P6_1PASSWORD_PROFILE
+#  Environment:	 P6_DFZ_PROFILE_1PASSWORD
 #>
 ######################################################################
 p6df::modules::home::profile::1password() {
 
-  p6_env_export "P6_1PASSWORD_PROFILE" "Gollucci"
+  p6_env_export "P6_DFZ_PROFILE_1PASSWORD" "P6"
 
   p6_1password_account_signin "my"
   p6_1password_whoami_email
@@ -86,12 +83,12 @@ p6df::modules::home::profile::1password() {
 #
 # Function: p6df::modules::home::profile::aws()
 #
-#  Environment:	 P6_AWS_ORG P6_AWS_PROFILE
+#  Environment:	 P6_AWS_ORG P6_DFZ_PROFILE_AWS
 #>
 ######################################################################
 p6df::modules::home::profile::aws() {
 
-   p6_env_export "P6_AWS_PROFILE" "p6m7g8"
+   p6_env_export "P6_DFZ_PROFILE_AWS" "P6"
 
    p6_env_export P6_AWS_ORG "p6"
    p6_aws_cli_organization_on "$P6_AWS_ORG"
@@ -105,19 +102,22 @@ p6df::modules::home::profile::aws() {
 #
 # Function: p6df::modules::home::profile::github()
 #
-#  Environment:	 GH_CONFIG_DIR GH_TOKEN P6_GITHUB_PROFILE XXX
+#  Environment:	 GH_CONFIG_DIR GH_TOKEN GH_USER GITHUB_MCP_PATH HOME P6_DFZ_PROFILE_GITHUB P6_GITHUB_PROFILE
 #>
 ######################################################################
 p6df::modules::home::profile::github() {
 
-  p6_env_export "P6_GITHUB_PROFILE" "Personal/pgollucci"
+  p6_env_export "P6_DFZ_PROFILE_GITHUB" "P6"
 
-  local user=$(p6_1password_cli_item_get_username "Github")
-  local token=$(p6_1password_cli_item_get_password "Github")
+  local user=$(p6_1password_cli_item_get_username "Github/pgollucci/gh_token/classic")
+  local token=$(p6_1password_cli_item_get_api_key "Github/pgollucci/gh_token/classic")
 
-  # GH_CONFIG_DIR ~/.config/gh
-  # XXX: Maintain symlinks like aws
+  p6_env_export "GH_CONFIG_DIR" "$HOME/.config/gh-${P6_GITHUB_PROFILE}"
+  p6_env_export "GH_USER" "$user"
   p6_env_export "GH_TOKEN" "$token"
+
+  local gemini_token=$(p6_1password_cli_item_get_api_key "Github/pgollucci/gemini/pat")
+  p6_env_export GITHUB_MCP_PATH "$gemini_token"
 
   p6_return_void
 }
@@ -127,15 +127,17 @@ p6df::modules::home::profile::github() {
 #
 # Function: p6df::modules::home::profile::npm()
 #
-#  Environment:	 NPM NPM_TOKEN P6_NPM_PROFILE
+#  Environment:	 NPM_TOKEN NPM_USER P6_DFZ_PROFILE_NPM
 #>
 ######################################################################
 p6df::modules::home::profile::npm() {
 
-  p6_env_export "P6_NPM_PROFILE" "Personal/pgollucci"
+  p6_env_export "P6_DFZ_PROFILE_NPM" "P6"
 
-  local token=$(p6_1password_cli_item_get_password "NPM")
+  local user="pgollucci"
+  local token=$(p6_1password_cli_item_get_api_key "Npmjs/npm_token")
   p6_env_export NPM_TOKEN "$token"
+  p6_env_export NPM_USER "$user"
 
   p6_return_void
 }
@@ -145,12 +147,12 @@ p6df::modules::home::profile::npm() {
 #
 # Function: p6df::modules::home::profile::launchdarkly()
 #
-#  Environment:	 P6_LAUNCHDARKLY_PROFILE
+#  Environment:	 P6_DFZ_PROFILE_LAUNCHDARKLY
 #>
 ######################################################################
 p6df::modules::home::profile::launchdarkly() {
 
-  p6_env_export "P6_LAUNCHDARKLY_PROFILE" "Personal/"
+  p6_env_export "P6_DFZ_PROFILE_LAUNCHDARKLY" "P6"
 
   p6_return_void
 }
@@ -160,12 +162,12 @@ p6df::modules::home::profile::launchdarkly() {
 #
 # Function: p6df::modules::home::profile::pagerduty()
 #
-#  Environment:	 P6_PAGERDUTY_PROFILE
+#  Environment:	 P6_DFZ_PROFILE_PAGERDUTY
 #>
 ######################################################################
 p6df::modules::home::profile::pagerduty() {
 
-  p6_env_export "P6_PAGERDUTY_PROFILE" "Personal/"
+  p6_env_export "P6_DFZ_PROFILE_PAGERDUTY" "P6"
 
   p6_return_void
 }
@@ -175,12 +177,62 @@ p6df::modules::home::profile::pagerduty() {
 #
 # Function: p6df::modules::home::profile::vscode()
 #
-#  Environment:	 P6_VSCODE_PROFILE
+#  Environment:	 P6_DFZ_PROFILE_VSCODE
 #>
 ######################################################################
 p6df::modules::home::profile::vscode() {
 
-  p6_env_export "P6_VSCODE_PROFILE" "Personal"
+  p6_env_export "P6_DFZ_PROFILE_VSCODE" "P6"
+
+  p6_return_void
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::home::profile::openai()
+#
+#  Environment:	 P6_DFZ_PROFILE_OPENAI
+#>
+######################################################################
+p6df::modules::home::profile::openai() {
+
+  p6_env_export "P6_DFZ_PROFILE_OPENAI" "P6"
+
+  eval "$(p6_1password_cli_item_get_code OpenAI Private)"
+
+  p6_return_void
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::home::profile::gemini()
+#
+#  Environment:	 P6_DFZ_PROFILE_GEMINI
+#>
+######################################################################
+p6df::modules::home::profile::gemini() {
+
+  p6_env_export "P6_DFZ_PROFILE_GEMINI" "P6"
+
+  p6_return_void
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::home::profile::claude()
+#
+#  Environment:	 CLAUDE_CODE_OAUTH_TOKEN P6_DFZ_PROFILE_CLAUDE
+#>
+######################################################################
+p6df::modules::home::profile::claude() {
+
+  p6_env_export "P6_DFZ_PROFILE_CLAUDE" "P6"
+
+  local token=$(p6_1password_cli_item_get_api_key "Claude Code/oauth_token")
+  p6_env_export "CLAUDE_CODE_OAUTH_TOKEN" "$token"
 
   p6_return_void
 }
