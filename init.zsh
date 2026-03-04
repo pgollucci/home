1a7a8ca9d4721ae88bcdb101b27df2e688d12b0a
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-03-03 23:57:57 -0500

    feat: add confluence and figma modules to user shell config (#26)
    * test: add confluence and figma modules to user shell config
    
    * feat: satisfy shellcheck for init.zsh in module loader branch
    
    * feat: align workflow yaml formatting with repo lint rules
    
    * feat: satisfy workflow yamllint for remaining PR automation files
    
    * feat: quote symlink paths to satisfy shellcheck in CI

--- init.zsh
+++ init.zsh
@@ -1,3 +1,5 @@
+# shellcheck shell=bash
+
 ######################################################################
 #<
 #
@@ -21,16 +23,16 @@ p6df::modules::home::deps() {
 ######################################################################
 p6df::modules::home::home::symlink() {
 
-  ln -fs $P6_DFZ_SRC_DIR/pgollucci/home/README.md README.md
-  ln -fs $P6_DFZ_SRC_DIR/pgollucci/home/LICENSE LICENSE
+  ln -fs "$P6_DFZ_SRC_DIR/pgollucci/home/README.md" README.md
+  ln -fs "$P6_DFZ_SRC_DIR/pgollucci/home/LICENSE" LICENSE
 
-  ln -fs $P6_DFZ_SRC_DIR/pgollucci/home/.fullname .fullname
-  ln -fs $P6_DFZ_SRC_DIR/pgollucci/home/.hosts .hosts
-  ln -fs $P6_DFZ_SRC_DIR/pgollucci/home/.organization .organization
-  ln -fs $P6_DFZ_SRC_DIR/pgollucci/home/.signature .signature
+  ln -fs "$P6_DFZ_SRC_DIR/pgollucci/home/.fullname" .fullname
+  ln -fs "$P6_DFZ_SRC_DIR/pgollucci/home/.hosts" .hosts
+  ln -fs "$P6_DFZ_SRC_DIR/pgollucci/home/.organization" .organization
+  ln -fs "$P6_DFZ_SRC_DIR/pgollucci/home/.signature" .signature
 
-  ln -fs $P6_DFZ_SRC_DIR/pgollucci/home-private/ssh .ssh
-  ln -fs $P6_DFZ_SRC_DIR/pgollucci/home-private/gnupg .gnupg
+  ln -fs "$P6_DFZ_SRC_DIR/pgollucci/home-private/ssh" .ssh
+  ln -fs "$P6_DFZ_SRC_DIR/pgollucci/home-private/gnupg" .gnupg
 }
 
 ######################################################################
